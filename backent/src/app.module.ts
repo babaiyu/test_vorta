a95465b9033fb191ce498f9ea33b7cbf4eae7f1c
babaiyu <bayuputra9a@gmail.com>
2022-02-05 11:08:07 +0700

    Setting up TypeORM & connect to database, import UsersModule

--- backent/src/app.module.ts
+++ backent/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersEntity } from './users/users.entity';
+import { UsersModule } from './users/users.module';
+import env from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: env.HOST,
+      port: env.PORT,
+      username: env.USERNAME,
+      password: env.PASSWORD,
+      database: env.DATABASE,
+      entities: [UsersEntity],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
