562101bb46728f16cccd52c0f0a4912bd8fd65d3
babaiyu <bayuputra9a@gmail.com>
2022-02-09 10:02:31 +0700

    Added services for addPatient, getTreatments, and getLocations

--- frontent/src/services/api.ts
+++ frontent/src/services/api.ts
@@ -10,3 +10,15 @@ export const changePatientStatus = async (id: number, status: boolean) => {
   const payload = { isActive: status };
   return await axios.patch(`${URL}/patients/changeStatus/${id}`, payload);
 };
+
+export const addPatient = async (payload: any) => {
+  return await axios.post(`${URL}/patients`, payload);
+};
+
+export const getTreatments = async () => {
+  return await axios.get(`${URL}/treatments`);
+};
+
+export const getLocations = async () => {
+  return await axios.get(`${URL}/locations`);
+};
