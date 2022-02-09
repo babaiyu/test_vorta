9d4ca1b14226da323511e68d2a5b79e2c9c84d96
babaiyu <bayuputra9a@gmail.com>
2022-02-10 06:43:14 +0700

    Added hooks for update & delete patient

--- frontent/src/hooks/patientsHook.ts
+++ frontent/src/hooks/patientsHook.ts
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import { addPatient, changePatientStatus, getPatients } from "../services/api";
+import {
+  addPatient,
+  changePatientStatus,
+  deletePatient,
+  getPatients,
+  updatePatient,
+} from "../services/api";
 
 export default function patientsHook() {
   const [patients, setPatients] = useState([]);
@@ -33,9 +39,31 @@ export default function patientsHook() {
     }
   };
 
+  const _updatePatient = async (id: number, value: any) => {
+    try {
+      const result = await updatePatient(id, value);
+      if (result.data) await onGetPatients();
+      return true;
+    } catch (error) {
+      console.log("Error => ", error);
+      return false;
+    }
+  };
+
+  const _deletePatient = async (id: number) => {
+    try {
+      const result = await deletePatient(id);
+      if (result.data) await onGetPatients();
+      return true;
+    } catch (error) {
+      console.log("Error => ", error);
+      return false;
+    }
+  };
+
   useEffect(() => {
     onGetPatients();
   }, []);
 
-  return { patients, changeStatus, _addPatient };
+  return { patients, changeStatus, _addPatient, _updatePatient, _deletePatient };
 }
