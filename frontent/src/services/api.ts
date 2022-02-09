9546d218f077048e7d6650b3c111bc1109b074c8
babaiyu <bayuputra9a@gmail.com>
2022-02-10 06:42:45 +0700

    Added service updatePatient & deletePatient

--- frontent/src/services/api.ts
+++ frontent/src/services/api.ts
@@ -15,6 +15,14 @@ export const addPatient = async (payload: any) => {
   return await axios.post(`${URL}/patients`, payload);
 };
 
+export const updatePatient = async (id: number, payload: any) => {
+  return await axios.patch(`${URL}/patients/${id}`, payload);
+};
+
+export const deletePatient = async (id: number) => {
+  return await axios.delete(`${URL}/patients/${id}`);
+};
+
 export const getTreatments = async () => {
   return await axios.get(`${URL}/treatments`);
 };
